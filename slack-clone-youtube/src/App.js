e8696fbc738a810138a227dd45b9da743232d61b
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-27 00:57:39 +0530

    day end work

--- slack-clone-youtube/src/App.js
+++ slack-clone-youtube/src/App.js
@@ -1,12 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
+import Header from './Header';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="app">
+      <Router>
+        <div>
+          {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+          <Switch>
+            <Route path="/" exact>
+              <Header />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
     </div>
   );
 }
