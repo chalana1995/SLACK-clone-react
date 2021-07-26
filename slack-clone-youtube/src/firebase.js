985fafbb588e38d0c32ecf27c3b3837245242092
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-26 22:46:45 +0530

    add firebase configeration

---
+++ slack-clone-youtube/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyDfEmyaH1PjFRtqMCekQmKdeB3vs_VfVx4",
+    authDomain: "slack-clone-yt-228eb.firebaseapp.com",
+    projectId: "slack-clone-yt-228eb",
+    storageBucket: "slack-clone-yt-228eb.appspot.com",
+    messagingSenderId: "367367806011",
+    appId: "1:367367806011:web:7d8e6b519cea84335a101e",
+    measurementId: "G-61LJGEX6KZ"
+  };
